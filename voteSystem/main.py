621977973f542adc088d2a3c07775a322e1da64a
taojy123 <taojy123@163.com>
2016-08-07 01:54:06 +0800

    output xls file

--- voteSystem/main.py
+++ voteSystem/main.py
@@ -1,21 +1,28 @@
-from bottle import route, run, abort, static_file, template, get, post, request, response, redirect, view
+import StringIO
+
+import BeautifulSoup
+import xlwt
+
+from bottle import route, run, abort, static_file, template, get, post, request, response, redirect, view, hook
 import bottle
 from common import configReader
 from common.operMapping import OperClassNameList
-from bottle.ext import beaker
+import bottle_beaker as beaker
 from oper.adminOper import AdminOper
+
+
+@hook('before_request')
+def strip_path():
+    request.environ['PATH_INFO'] = request.environ['PATH_INFO'].rstrip('/')
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root='./static')
 
 
-@route('/admin/:funcName', method="GET")
-@route('/admin/:funcName', method="POST")
-@route('/admin/:funcName/', method="GET")
-@route('/admin/:funcName/', method="POST")
-
-def admin_oper(funcName = ''):
+@route('/admin/:funcName', method=["GET", "POST"])
+def admin_oper(funcName=''):
     global g_operClassObj
 
     operClassObj = g_operClassObj.get('AdminOper', None)
@@ -35,6 +42,55 @@ def initOperClass():
 
 def getClass(className):
     return eval(className + '()')
+
+
+@post('/output', method="POST")
+def output():
+    data = request.params.data
+    begin_index = int(request.params.get('begin_index', 0))
+    end_index = int(request.params.get('end_index', -1))
+
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('output')
+
+    soup = BeautifulSoup.BeautifulSoup(data)
+
+    thead_soup = soup.find('thead')
+    th_soups = thead_soup.findAll('th')
+    th_soups = th_soups[begin_index:end_index]
+
+    j = 0
+    for th_soup in th_soups:
+        th = th_soup.getText()
+        ws.write(0, j, th)
+        j += 1
+
+    tbody_soup = soup.find('tbody')
+    tr_soups = tbody_soup.findAll('tr')
+
+    i = 1
+    for tr_soup in tr_soups:
+        td_soups = tr_soup.findAll('td')
+        td_soups = td_soups[begin_index:end_index]
+
+        j = 0
+        for td_soup in td_soups:
+            td = td_soup.getText()
+            ws.write(i, j, td)
+            j += 1
+
+        i += 1
+
+    s = StringIO.StringIO()
+    wb.save(s)
+    s.seek(0)
+    data = s.read()
+
+    response.headers['Content-Type'] = 'application/octet-stream'
+    response.headers['Content-Disposition'] = 'attachment;filename="output.xls"'
+
+    return data
+
 
 app = beaker.middleware.SessionMiddleware(bottle.app(), configReader.GetSystemConfig().session_opts)
 
